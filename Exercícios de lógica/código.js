a0e8c5ebe799902916aba820b6cacbfdf216ad4e
hannaakellyy <hannakellyoliver@gmail.com>
2026-04-12 14:32:05 -0300

    atualizado 2.2

--- Exercícios de lógica/código.js
+++ Exercícios de lógica/código.js
@@ -17,3 +17,22 @@ else {
 }
 console.log ( nome, "seu total é:", total );
 }
+
+// exercício 2
+
+const itens = ["1","2","3"];
+for (let i = 0; i < itens.length; i++ ) {
+    switch (itens [i]) {
+        case "1":
+        console.log ("fácil");
+        break;
+        case "2":
+        console.log ("médio");
+        break;
+        case "3":
+        console.log ("dificíl");
+        break;
+        default:
+        console.log ("item não encontrado");
+    }
+}
